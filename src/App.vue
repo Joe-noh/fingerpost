b98a0ce8a3864528fc6e139295a396ef2a45fc95
Joe-noh <goflb.jh@gmail.com>
2017-02-01 00:13:36 +0900

    draft store, router, filters and views

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="app">
+      <router-view class="router-view"></router-view>
+    </div>
+</template>
+
+<style lang="sass">
+  .router-view {
+    max-width: 960px
+    margin: 0 auto
+    position: relative
+  }
+</style>
---
+++ src/views/HelloView.vue
@@ -0,0 +1,12 @@
+<template>
+  <p>Hello</p>
+</template>
+
+<script>
+  export default {
+    name: 'hello-view',
+    preFetch: (store) => {
+      console.log(store);
+    }
+  };
+</script>
