e7dcff4e339692c47b395e268502aa43a5bdf0f0
Joe-noh <honzawa.j@pepabo.com>
2017-02-01 08:43:12 +0900

    syakyo setup-dev-server.js

---
+++ src/build/setup-dev-server.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const MFS = require('memory-fs');
+const clientConfig = require('./webpack.client.config');
+const serverConfig = require('./webpack.server.config');
+
+module.exports = function setupDevServer(app, opts) {
+  clientConfig.entry.app = ['webpack-hot-middleware/client', clientConfig.entry.app];
+  clientConfig.output.filename = '[name].js';
+  clientConfig.plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  );
+
+  const clientCompiler = webpack(clientConfig);
+  const devMiddleware = webpackDevMiddleware(clientCompiler, {
+    publicPath: clientConfig.output.publicPath,
+    stats: {
+      colors: true,
+      chunks: false
+    }
+  });
+
+  app.use(devMiddleware);
+
+  clientCompiler.plugin('done', () => {
+    const fs = devMiddleware.fileSystem;
+    const filePath = path.join(clientConfig.output.path, 'index.html');
+
+    if (fs.existsSyn(filePath)) {
+      const index = fs.readFileSync(filePath, 'utf-8');
+      opts.indexUpdated(index);
+    }
+  });
+
+  app.use(webpackHotMiddleware(clientCompiler));
+
+  const serverCompiler = webpack(serverConfig);
+  const mfs = new MFS();
+  const outputPath = path.join(serverConfig.output.path, serverConfig.output.filename);
+
+  serverCompiler.outputFileSystem = mfs;
+  serverCompiler.watch({}, (err, stats) => {
+    if (err) { throw err; }
+
+    stats = stats.toJSON();
+    stats.errors.forEach(err => console.error(err));
+    stats.warnings.forEach(warn => console.warn(warn));
+
+    opts.bundleUpdated(mfs.readFileSync(outputPath, 'utf-8'));
+  });
+}
