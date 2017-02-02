8b1e72359972668d637627e068de846894e5be80
Joe-noh <goflb.jh@gmail.com>
2017-02-02 23:05:40 +0900

    fix typo; not warning, warnings

--- build/webpack.client.config.js
+++ build/webpack.client.config.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === 'production') {
     }),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
-        warning: false
+        warnings: false
       }
     }) //,
     // new SWPrecachePlugin({
