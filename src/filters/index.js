ec0599bdbeab00845a207223401ee2d8f28c853b
Joe-noh <honzawa.j@pepabo.com>
2017-02-02 21:03:22 +0900

    so lough templating

--- src/filters/index.js
+++ src/filters/index.js
@@ -1,1 +1,18 @@
-export function foo(something) {}
+export function upcase(string) {
+  return string.toUpperCase();
+}
+
+export function definitionId(ref) {
+  return `#${definitionModelName(ref)}`;
+}
+
+export function definitionModelName(ref) {
+  if (ref) {
+    let splitted = ref.split('/');
+    let modelName = splitted[splitted.length - 1];
+
+    return modelName;
+  } else {
+    return "";
+  }
+}
