9390fc9baca4ed28f86a0907f31b0386ab3e380a
Joe-noh <honzawa.j@pepabo.com>
2017-02-16 15:48:11 +0900

    test that given params is stored as properties

--- test/fingerpost_test.js
+++ test/fingerpost_test.js
@@ -2,7 +2,21 @@ import Fingerpost from '../src/lib/fingerpost';
 import {expect} from 'chai';
 
 describe('Fingerpost', () => {
-  it('is ok', () => {
-    expect(Fingerpost).to.equal(Fingerpost);
+  describe('#constructor', () => {
+    let fingerpost;
+    let url = 'https://example.com/a.json';
+    let template = './path/to/template';
+
+    beforeEach(() => {
+      fingerpost = new Fingerpost({url, template});
+    });
+
+    it('should have member `url`', () => {
+      expect(fingerpost.url).to.equal(url);
+    });
+
+    it('should have member `template`', () => {
+      expect(fingerpost.template).to.equal(template);
+    });
   });
 });
