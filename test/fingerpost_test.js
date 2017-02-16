141cb98ddaf0d562d8b573543639fc77a82a8821
Joe-noh <honzawa.j@pepabo.com>
2017-02-16 14:17:51 +0900

    first test

---
+++ test/fingerpost_test.js
@@ -0,0 +1,8 @@
+import Fingerpost from '../src/lib/fingerpost';
+import assert from 'assert';
+
+describe('Fingerpost', () => {
+  it('is ok', () => {
+    assert.equal(Fingerpost, Fingerpost);
+  });
+});
