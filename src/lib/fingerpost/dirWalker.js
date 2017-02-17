a9b49ccfd44fc90a792112aca4e8511cbf5d9446
Joe-noh <goflb.jh@gmail.com>
2017-02-17 23:06:41 +0900

    implement recursive coping

---
+++ src/lib/fingerpost/dirWalker.js
@@ -0,0 +1,29 @@
+import path from 'path';
+import fs from 'fs';;
+
+class DirWalker {
+  constructor() {}
+
+  copy(src, dest, transformer) {
+    if (!fs.existsSync(src)) { return; }
+
+    let stats = fs.statSync(src);
+    if (stats.isDirectory()) {
+      fs.mkdirSync(dest);
+
+      let files = fs.readdirSync(src);
+      files.forEach(file => {
+        this.copy(path.join(src, file), path.join(dest, file), transformer);
+      });
+    } else {
+      if (transformer) {
+        let content = transformer(src);
+        fs.writeFileSync(dest, content);
+      } else {
+        fs.linkSync(src, dest);
+      }
+    }
+  }
+}
+
+export default DirWalker;
---
+++ test/dirWalkerTest.js
@@ -0,0 +1,36 @@
+import DirWalker from '../src/lib/fingerpost/dirWalker';
+import {expect} from 'chai';
+import rimraf from 'rimraf';
+import fs from 'fs';
+import path from 'path';
+
+describe('DirWalker', () => {
+  let walker;
+  let src = path.join(__dirname, 'fixtures');
+  let dest = path.join('/', 'tmp', 'fp');
+
+  beforeEach(() => {
+    walker = new DirWalker();
+  });
+
+  describe('#copy', () => {
+    beforeEach(() => {
+      rimraf.sync(dest);
+    });
+
+    it('copies files keeping directory structure', () => {
+      walker.copy(src, dest);
+
+      expect(fs.existsSync(path.join(dest, 'a'))).to.eql(true);
+      expect(fs.existsSync(path.join(dest, 'a.txt'))).to.eql(true);
+      expect(fs.existsSync(path.join(dest, 'a', 'b.txt'))).to.eql(true);
+    });
+
+    it('transforms file content', () => {
+      walker.copy(src, dest, (src => 'hey'));
+
+      expect(fs.readFileSync(path.join(dest, 'a.txt'), 'utf8')).to.eql('hey');
+      expect(fs.readFileSync(path.join(dest, 'a', 'b.txt'), 'utf8')).to.eql('hey');
+    })
+  });
+});
