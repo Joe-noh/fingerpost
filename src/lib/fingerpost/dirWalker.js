f2dff11da52f8734b380b6b4591fcb177d1d1065
Joe-noh <goflb.jh@gmail.com>
2017-02-17 23:22:14 +0900

    add only option
    if only(src) returns true, it will be copied

--- src/lib/fingerpost/dirWalker.js
+++ src/lib/fingerpost/dirWalker.js
@@ -16,6 +16,8 @@ class DirWalker {
         this.copy(path.join(src, file), path.join(dest, file), opts);
       });
     } else {
+      if (opts.only && !opts.only(src)) { return; }
+
       if (opts.transform) {
         let content = opts.transform(src);
         fs.writeFileSync(dest, content);
--- test/dirWalkerTest.js
+++ test/dirWalkerTest.js
@@ -31,6 +31,14 @@ describe('DirWalker', () => {
 
       expect(fs.readFileSync(path.join(dest, 'a.txt'), 'utf8')).to.eql('hey');
       expect(fs.readFileSync(path.join(dest, 'a', 'b.txt'), 'utf8')).to.eql('hey');
-    })
+    });
+
+    it('copies only the condition met', () => {
+      walker.copy(src, dest, {only: (src => path.basename(src) == 'b.txt')});
+
+      expect(fs.existsSync(path.join(dest, 'a'))).to.eql(true);
+      expect(fs.existsSync(path.join(dest, 'a.txt'))).to.eql(false);
+      expect(fs.existsSync(path.join(dest, 'a', 'b.txt'))).to.eql(true);
+    });
   });
 });
